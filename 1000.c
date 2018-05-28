66cfcbbd8079d6c63d7b212cd62c4297a7a021a5
Dip Ghosh <39679672+Dip-Ghosh@users.noreply.github.com>
2018-05-29 02:39:35 +0600

    Timus problem Solution no 1000

---
+++ 1000.c
@@ -0,0 +1,14 @@
+     /* Author:Dip Ghosh
+        Written:28.05.2017
+        For C programming course practice work
+    */
+    #include<stdio.h>
+    int main()
+    {
+
+        int a,b;
+        scanf("%d%d",&a,&b);
+        printf("%d",a+b);
+        return 0;
+
+    }

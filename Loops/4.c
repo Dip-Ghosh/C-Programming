779a159d13587fe9d7a9ac94741d6f891954c0c9
Dip Ghosh <39679672+Dip-Ghosh@users.noreply.github.com>
2018-10-21 01:55:09 +0600

    problems solutions

--- Loops/4.c
+++ Loops/4.c
@@ -1,19 +1,13 @@
+//1+(2+3)+(4+5+6)+...........
 #include<stdio.h>
 #include<math.h>
 int main()
 {
 
-     int i=1,n,sum=0,sum1=0;
+     int i=1,n,sum=0;
      scanf("%d",&n);
-     for(i=1;i<=n;i++)
-     {  sum=0,sum1=0;
-         for(int j=1;j<=i;j++)
-         {
-             sum=sum+j;
-         }
-         sum1=sum1+sum;
-     }
-     printf("%d",sum1);
+     sum=(n*(n+1)*(pow(n,2)+n+2))/8;
+     printf("%d",sum);
      return 0;
 }
 
